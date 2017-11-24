8388bc8872e8a6ed254c7691b822021efb1f25cb
chaoqunzhan <31342350+chaoqunzhan@users.noreply.github.com>
2017-11-24 12:43:59 +0800

    javascript

--- Java Script execise/JavaScript动态网页教程/20171123/print_the_calendar.js
+++ Java Script execise/JavaScript动态网页教程/20171123/print_the_calendar.js
@@ -14,7 +14,7 @@ function isBissextile(year)
 //在网页中打印year和month月份的日历
 function printCalendar(year,month){
 	var aDate=new Date(year,month,1);   //创建year年month月1日的对象，并把实例的引用赋值给变量aDate
-	var week=aDate.getDay();     //aDate调用getDay()获取星期的信息
+	var week=aDate.getDay();     //aDate调用getDay()获取星期的信息，0表示星期一
 	//计算month月份的天数
 	var numOfMonth;
 	if(month==0||month==2||month==4||month==6||month==7||month==9||month==11){
@@ -31,24 +31,24 @@ function printCalendar(year,month){
 
 
 //创建一个打印日期的数组
-	var darArray=new Array(week+numOfMonth);
+	var dayArray=new Array(week+numOfMonth);      //打印空格，使得星期和日期能够对应
 	for(var i=0;i<week;i++){
 		dayArray[i]="";
 	}
 
-	for(var i=0,j=1;i<dayArray.length;i++,j++){
+	for(var i=week,j=1;i<dayArray.length;i++,j++){     //打印相应的日期
 		dayArray[i]=""+j;
 	}
 
 //打印日历表
 	document.write("<table>");
 	document.write("<tr><th colspan=7>"+year+"年"+(month+1)+"月</th></tr>");
-	document.write("<tr><th>日</th></tr><tr><th>一</th></tr><tr><th>二</th></tr><tr><th>三</th></tr><tr><th>四</th></tr><tr><th>五</th></tr><tr><th>六</th></tr>");
-	for(var i=0;i<dayArray.length;i+=7)
+	document.write("<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>");
+	for(var i=0;i<dayArray.length;i+=7)            //i+=7等价于i=i+7,i+=7可以自动转换成左边的数值类型
 		{
 			document.write("<tr>");
 			for(var j=i;j<dayArray.length&&j<i+7;j++){
-				document.write("<td>"+dayArray[i]+"</td>");
+				document.write("<td>"+dayArray[j]+"</td>");
 			}
 			document.write("</tr>");
 		}
