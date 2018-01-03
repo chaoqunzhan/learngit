9aafba38582da8eeabb0d62f4395807fe0ab890b
chaoqunzhan <31342350+chaoqunzhan@users.noreply.github.com>
2018-01-03 15:17:08 +0800

    php

---
+++ PHP/留言板/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=GB2312">
+<title>留言板</title>
+<style type="text/css"></style>
+</head>
+<body>
+	<table width="100%" border="0" cellpadding="2" cellspacing="2" bgcolor="#cccccc">
+	<tr>
+	<td width="8%"><div align="center">编号</div></td>
+	<td width="19%"><div align="center">留言者</div></td>	
+	<td width="58%"><div align="center">主题</div></td>	
+	<td width="15%"><div align="center">日期</div></td>			
+	</tr>
+	<tr bgcolor="#ffffff">
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	<td>&nbsp;</td>
+	</tr>
+	<tr>
+	<td colspan="4">&nbsp;</td>
+	</tr>
+	</table>
+</body>
+</html>
